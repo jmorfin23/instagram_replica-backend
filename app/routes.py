a5f05257a7e05329367c77e3c01210b4602fe80a
Jonathan Morfin <jmorfin776@gmail.com>
2019-11-07 13:18:52 -0500

    user auth complete

--- app/routes.py
+++ app/routes.py
@@ -17,8 +17,6 @@ def register():
     try:
         token = request.headers.get('token')
 
-
-
         # decode the token back to a dictionary
         data = jwt.decode(
         token,
@@ -27,7 +25,6 @@ def register():
         )
         print(data)
 
-
         #create the user
         user = User(email=data['email'], fullname=data['name'], username=data['username'])
         user.set_password(data['password'])
@@ -35,18 +32,32 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        return jsonify({ 'Success': 'User created!'})
+        return jsonify({ 'message': 'success', 'User': 'Success User Created!' })
     except:
         return jsonify({ 'Error_001': 'User not created, try again.'})
 
 
 @app.route('/api/login', methods=['GET', 'POST'])
 def login():
-    print('*****************')
-    print('*****************')
-    print('*****************')
-    print('inside login API')
-    print('*****************')
-    print('*****************')
-    print('*****************')
-    return jsonify({ 'Success': 'There was something returned!'})
+
+    token = request.headers.get('token')
+
+    print(token)
+
+    #decrypt the token
+    data = jwt.decode(
+    token,
+    app.config['SECRET_KEY'],
+    algorithm=['HS256']
+    )
+
+    print(data)
+
+    #check if user is in database
+    user = User.query.filter_by(email=data['email']).first()
+
+    if user is None or not user.check_password(data['password']):
+        return jsonify({ 'message': 'Error #002: Invalid credentials' })
+
+    # create a token and return it
+    return jsonify({ 'message': 'success', 'token': user.get_token() })
